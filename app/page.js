dd2b0bf53698d8504f4e13c077d4a6031ed6eb05
Uzair Aslam <uzairaslam990@gmail.com>
2024-03-29 04:20:51 -0700

    event onChange and onClick apply on form

--- app/page.js
+++ app/page.js
@@ -3,7 +3,38 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
-  
+  let [data,setData]=useState([])
+  const start={
+    name:"",
+    email:"",
+    password:"",
+    gender:"",
+    number:0,
+    course:"",
+    degree:"",
+    address:"",
+    profilePicture:null,
+    file:null
+  }
+  let [value,setValue]=useState(start)
+  const onChangeHandler = (e) => {
+    if (e.target.type === "file") {
+      setValue((a) => ({
+        ...a,
+        [e.target.name]: e.target.files[0]
+      }));
+    } else {
+      setValue((a) => ({
+        ...a,
+        [e.target.name]: e.target.value
+      }));
+    }
+  };
+  const clickHandler=(e)=>{
+    e.preventDefault()
+    setData((a)=>[...a,value])
+    setValue(start)
+  }
   return (
     <div>
     <div className="max-w-xl mx-auto p-6">
